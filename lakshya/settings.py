bd4d4565d1f077d5b76ac96068f206a166726f0a
luhpelayo <luhpelayo@gmail.com>
2023-10-01 20:42:25 -0400

    configuracion con el servidor

--- lakshya/settings.py
+++ lakshya/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hk-ez4*0s9$ufht5el_8(d)d(&xlm8swqudx&*duza&z*5tv@('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['44.204.195.135', 'localhost', '127.0.0.1']
 
 
 # Application definition
